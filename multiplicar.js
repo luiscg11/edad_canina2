ca7b6cd7ad8d745ce6dd7a86c1e8f1fe9138390c
luiscg11 <luicg91gmail.com>
2018-03-25 18:30:18 -0600

    primer commit
    mi primer commit

---
+++ multiplicar.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+var edad_humana = prompt("Intruduzca un numero: ");
+var edad_canina = edad_humana * 7;
+	document.write("la edad humana que usted ingreso equivale a \ "+ edad_canina +" años edad canina");
+}
